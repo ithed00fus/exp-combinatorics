f4362a897eea089d88d1b6651d4686170237bc99
Madhav Lolla <siva.lolla@gmail.com>
2016-02-25 14:39:27 -0500

    apocalyose aded

---
+++ Combinatorics/zombie-apocalypse/project.clj
@@ -0,0 +1,8 @@
+(defproject zombie-apocalypse "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [incanter/incanter-core "1.5.7"]
+                 [incanter/incanter-io "1.5.7"]])
+(use 'incanter.core
+     'incanter.io)                                  
